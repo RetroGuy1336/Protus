dfc568e90a276141c04ff370c79a926bdad5fbb9
RetroGuy1336 <arceniokalebe@gmail.com>
2026-04-20 14:53:02 -0300

    Some changes

--- config.py
+++ config.py
@@ -3,17 +3,21 @@ import argparse
 def arguments(command):
     parser = argparse.ArgumentParser(description="Protus parser")
 
-    parser.add_argument('comando', choices=['show'])
-    
+    subparsers = parser.add_subparsers(dest='comando')
+    show_parser = subparsers.add_parser('show')
+    show_parser.add_argument('--more', action="store_true")
 
-    args = parser.parse_args(command.split())
-
+    try:
+        args = parser.parse_args(command.split())
+    except SystemExit:
+        print("Command not found...")
+        return
+        
     if args.comando == 'show':
         print("""
 Protus, The Framework for Pentest
 Version: 0.1v
-Features: A framework for pentest
-""")
-    else:
-        print()
+Description: Protus, The Framework for Pentest is a tool to assist offensive cybersecurity engineers in their work""")
+    if args.more:
+        print("No")
         
