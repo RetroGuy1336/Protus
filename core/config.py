985c0361527fc1aaf1badfeeb2ff395baab52da6
RetroGuy1336 <arceniokalebe@gmail.com>
2026-04-24 18:10:35 -0300

    New commands

--- core/config.py
+++ core/config.py
@@ -12,12 +12,14 @@ def arguments(command):
     subparsers = parser.add_subparsers(dest='comando')
     show_parser = subparsers.add_parser('show', help="It shows some things of Protus")
     show_parser.add_argument('--more', action="store_true")
+    show_parser.add_argument("commands")
 
     list_parser = subparsers.add_parser("list")
     list_sub = list_parser.add_subparsers(dest="type")
 
     list_sub.add_parser("payloads")
     list_sub.add_parser("modules")
+    list_sub.add_parser("--more")
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     start_parser = subparsers.add_parser('start', help="It starts a module from Protus")
@@ -38,14 +40,22 @@ Version: 0.1.2v
 Description: Protus, The Framework for Pentest is a tool to assist offensive cybersecurity engineers in their work""")
         if args.more:
             print("No")
+        elif args.commands:
+            print("-=" * 58)
+            print("Current commands: show, show --more, show commands"
+                  "list payloads, list modules"
+                  "start recon, start exploitdb")
 
     if args.comando == "start":
+
         if args.modulo == "recon":
             ParserRecon()
-        if args.modulo == "exploitdb":
+
+        elif args.modulo == "exploitdb":
             ExploitDB_Parser()
     
     if args.comando == "list":
+
         if args.type == "payloads":
             pathway = "payloads"
 
@@ -56,5 +66,17 @@ Description: Protus, The Framework for Pentest is a tool to assist offensive cyb
                 print(file)
             print(limit)
             print("-=" * 20)
+
+        elif args.type == "modules":
+            pathway = "modules"
+
+            files = os.listdir(pathway)
+            print("-=" * 20)
+            print(red)
+            for file in files:
+                print(file)
+            print(limit)
+            print("-=" * 20)
+
 
         
